7cdd02bc613638931948f64571c4d821420c1ed8
Godwin C. Naces <157016869+GNaces@users.noreply.github.com>
2025-07-07 17:43:43 +0000

    minor adjustments

---
+++ script.js
@@ -0,0 +1,22 @@
+// Calculate and display age
+const birthYear = 1989;
+const currentYear = new Date().getFullYear();
+const age = currentYear - birthYear;
+document.getElementById('age').textContent = age;
+
+// Check access code for driver's license
+function checkAccessCode() {
+    const correctCode = "1989"; // Change this code if needed
+    const userInput = document.getElementById("codeInput").value;
+
+    if (userInput === correctCode) {
+        window.open("assets/photo/driver-license.jpeg", "_blank");
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000);
+    } else {
+        alert("Incorrect access code Mate, try again.");
+    }
+
+    return false;
+}
